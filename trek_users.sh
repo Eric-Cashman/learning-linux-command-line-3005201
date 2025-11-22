99f57d785063b512f993aa67f73b337221bc307b
Eric Cashman <118931515+Eric-Cashman@users.noreply.github.com>
2025-11-22 04:20:54 +0000

    Add trek_users.sh documentation for Star Trek character user creation

---
+++ trek_users.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Trek Users Creation Script
+# Star Trek Characters
+
+echo "Creating Trek users..."
+
+# Kirk
+./create_user.sh kirk abc123
+# Quote: "I want answers, Mister!"
+
+# Spock
+./create_user.sh spock abc123
+# Quote: "Highly illogical, Captain."
+
+# Uhura
+./create_user.sh uhura abc123
+# Quote: "Hailing frequencies open. Make it quick."
+
+echo "Trek users created successfully!"
